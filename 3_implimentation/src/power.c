b0b167524ec43b79c2ab58d2dc27d2c0e893c718
prathameshpatil505 <89597549+prathameshpatil505@users.noreply.github.com>
2021-09-07 20:01:44 +0530

    Delete 3_implimentation/src directory

--- 3_implimentation/src/power.c
+++
@@ -1,8 +0,0 @@
-#include "calculator.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-error_t power(arthimetic_inputs *ai){
-    ai->output=pow((ai->input_1),(ai->input_2));
-    return SUCCESS;
-}
